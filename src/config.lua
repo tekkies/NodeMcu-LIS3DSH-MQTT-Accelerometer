b85cd667070cf4196e0cf2e24da67dd4c492ca76
Andy Joiner <andy.joiner.0101@gmail.com>
2022-01-01 21:01:24 +0000

    5-minute sleep

--- src/config.lua
+++ src/config.lua
@@ -3,6 +3,6 @@ MQTT_BROKER = "test.mosquitto.org"
 MQTT_CLIENTID = "uk.co.tekkies." .. node.chipid()
 MQTT_TOPIC = "/tekkies.co.uk/LIS3DSH/" .. node.chipid() .. "-" .. node.flashid()
 USE_LED = false
-SLEEP_SECONDS = 5 --0 = continuous read
+SLEEP_SECONDS = 5*60 --0 = continuous read
 WAKE_SENSITIVITY = 64 --0-127
 ENABLE_PRINT = true
