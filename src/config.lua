60b48b22ba7deb209ef88273d9569603ced1097c
Andy Joiner <andy.joiner.0101@gmail.com>
2022-01-13 00:12:30 +0000

    Switch back to test.mosquitto.org

--- src/config.lua
+++ src/config.lua
@@ -1,6 +1,6 @@
-MQTT_BROKER = "192.168.10.10"
+MQTT_BROKER = "test.mosquitto.org"
 MQTT_CLIENTID = "uk.co.tekkies." .. node.chipid()
 USE_LED = false
-SLEEP_SECONDS = 15*60 --0 = continuous read
+SLEEP_SECONDS = 5*60 --0 = continuous read
 WAKE_SENSITIVITY = 64 --0-127
 ENABLE_PRINT = true
