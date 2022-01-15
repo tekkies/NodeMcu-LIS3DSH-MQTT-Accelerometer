66691f73ed18019bf53f6a2a1dc03af28ff273c9
Andy Joiner <andy.joiner.0101@gmail.com>
2022-01-15 22:47:23 +0000

    Pretty sensitive now

--- src/config.lua
+++ src/config.lua
@@ -2,5 +2,5 @@ MQTT_BROKER = "192.168.10.10"
 MQTT_CLIENTID = "uk.co.tekkies." .. node.chipid()
 USE_LED = true
 SLEEP_SECONDS = 5*60 --0 = continuous read
-WAKE_SENSITIVITY = 20 --0-127
+WAKE_SENSITIVITY = 2 --0-127
 ENABLE_PRINT = true
