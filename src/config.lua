cba70fc36611058d5f88b4e6817cf0d6b2df6869
Andy Joiner <andy.joiner.0101@gmail.com>
2022-01-16 00:17:26 +0000

    Disable LED, sleep for 15 minutes max

--- src/config.lua
+++ src/config.lua
@@ -1,6 +1,6 @@
 MQTT_BROKER = "192.168.10.10"
 MQTT_CLIENTID = "uk.co.tekkies." .. node.chipid()
-USE_LED = true
-SLEEP_SECONDS = 5*60 --0 = continuous read
+USE_LED = false
+SLEEP_SECONDS = 15*60 --0 = continuous read
 WAKE_SENSITIVITY = 2 --0-127
 ENABLE_PRINT = true
